ab9b80432dfd7c025d2da7b4c1ef93b6ed227d84
Jean Marcos <jean.silva552@gmail.com>
2022-05-27 14:29:28 -0300

    feat: add btns actions and Icons

--- nextjs/src/components/Table/index.tsx
+++ nextjs/src/components/Table/index.tsx
@@ -1,4 +1,5 @@
 import { Cliente } from '../../core/Cliente';
+import { IconeEdit, IconeTrash } from '../icons/index';
 
 interface TableProp {
   clientes: Cliente[];
@@ -7,11 +8,25 @@ interface TableProp {
 export function Table({ clientes }: TableProp) {
   function renderHead() {
     return (
-      <tr className="bg-gradient-to-r to-purple-400 from-purple-800">
+      <tr className="bg-gradient-to-r to-purple-400 from-purple-800 items-center">
         <td className="p-4  text-gray-100">ID</td>
         <td className="p-4  text-gray-100">Nome</td>
         <td className="p-4  text-gray-100">Idade</td>
+        <td className="p-4  text-gray-100">Ação</td>
       </tr>
+    );
+  }
+
+  function actions() {
+    return (
+      <td className="p-4">
+        <button className="text-green-500  hover:bg-white transition rounded-full p-1">
+          {IconeEdit}
+        </button>
+        <button className="text-red-600  hover:bg-white transition rounded-full p-1 ">
+          {IconeTrash}
+        </button>
+      </td>
     );
   }
 
@@ -28,9 +43,12 @@ export function Table({ clientes }: TableProp) {
         <td className="p-4 text-purple-800" key={item.id}>
           {item.age}
         </td>
+
+        {actions()}
       </tr>
     ));
   }
+
   return (
     <table className="w-full rounded-t-xl rounded-b-xl overflow-hidden">
       <thead className="">{renderHead()}</thead>
